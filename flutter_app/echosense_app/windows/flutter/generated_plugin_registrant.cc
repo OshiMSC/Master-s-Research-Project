9707ac7c20a34f906807d07c03597caa7e339c16
OshiMSC <oshadeedasanayake@gmail.com>
2026-06-11 23:12:12 +1200

    Store all commits

---
+++ flutter_app/echosense_app/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,29 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <battery_plus/battery_plus_windows_plugin.h>
+#include <flutter_ble_peripheral/flutter_ble_peripheral_plugin_c_api.h>
+#include <flutter_sound/flutter_sound_plugin_c_api.h>
+#include <geolocator_windows/geolocator_windows.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  BatteryPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
+  FlutterBlePeripheralPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBlePeripheralPluginCApi"));
+  FlutterSoundPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSoundPluginCApi"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+}
